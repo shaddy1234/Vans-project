c14fb79c93c32b0907f4e9de10bdf56b5d5a7d93
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-20 00:04:06 +0300

    Add vans link

--- index.jsx
+++ index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Vans from "./pages/Vans"
 
 function App() {
   return (
@@ -11,11 +12,13 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          < Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ pages/Vans.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Vans() {
+    return (
+        <div>
+            <h1>Van list</h1>
+        </div>
+    );
+}
+
+export default Vans;
