4298f3641361bfe73607435d54f405c29f37a818
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-20 11:05:06 +0300

    Add routes

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,36 +1,70 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-function Vans() {
-    const [vans, setVans] = useState([]);
+// function Vans() {
+//     const [vans, setVans] = useState([]);
 
-    useEffect(() => {
+//     useEffect(() => {
+//         fetch("/api/vans")
+//             .then(res => {
+//                 if (!res.ok) {
+//                     throw new Error('Network response was not ok');
+//                 }
+//                 return res.json();
+//             })
+//             .then(data => setVans(data.vans))
+//             .catch(error => console.error('Error fetching vans:', error));
+//     }, []);
+
+//     const vanElements = vans.map(van => (
+//         <div key={van.id} className="van-tile">
+//             <img src={van.imageUrl} alt={van.name} />
+//             <div className="van-info">
+//                 <h3>{van.name}</h3>
+//                 <p>${van.price}<span>/day</span></p>
+//             </div>
+//             <i className={`van-type ${van.type} selected`}>{van.type}</i>
+//         </div>
+//     ));
+
+//     return (
+//         <div>
+//             {vanElements}
+//         </div>
+//     );
+// }
+
+// export default Vans;
+
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function Vans() {
+    const [vans, setVans] = React.useState([])
+    React.useEffect(() => {
         fetch("/api/vans")
-            .then(res => {
-                if (!res.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return res.json();
-            })
+            .then(res => res.json())
             .then(data => setVans(data.vans))
-            .catch(error => console.error('Error fetching vans:', error));
-    }, []);
+    }, [])
 
     const vanElements = vans.map(van => (
         <div key={van.id} className="van-tile">
-            <img src={van.imageUrl} alt={van.name} />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price}<span>/day</span></p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            <Link to={`/vans/${van.id}`}>
+                <img src={van.imageUrl} />
+                <div className="van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
         </div>
-    ));
+    ))
 
     return (
-        <div>
-            {vanElements}
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
+            </div>
         </div>
-    );
+    )
 }
-
-export default Vans;
