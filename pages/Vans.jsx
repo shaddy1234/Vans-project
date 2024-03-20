baed667654f469bd52e1664d8e0bd49b0d19bd9b
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-20 12:42:35 +0300

    Use upgraded version

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,9 +1,9 @@
-// import React, { useState, useEffect } from 'react';
+// import React from "react"
+// import { Link } from "react-router-dom"
 
-// function Vans() {
-//     const [vans, setVans] = useState([]);
-
-//     useEffect(() => {
+// export default function Vans() {
+//     const [vans, setVans] = React.useState([])
+//     React.useEffect(() => {
 //         fetch("/api/vans")
 //             .then(res => {
 //                 if (!res.ok) {
@@ -12,29 +12,31 @@
 //                 return res.json();
 //             })
 //             .then(data => setVans(data.vans))
-//             .catch(error => console.error('Error fetching vans:', error));
 //     }, []);
+    
 
 //     const vanElements = vans.map(van => (
 //         <div key={van.id} className="van-tile">
-//             <img src={van.imageUrl} alt={van.name} />
-//             <div className="van-info">
-//                 <h3>{van.name}</h3>
-//                 <p>${van.price}<span>/day</span></p>
-//             </div>
-//             <i className={`van-type ${van.type} selected`}>{van.type}</i>
+//             <Link to={`/vans/${van.id}`}>
+//                 <img src={van.imageUrl} />
+//                 <div className="van-info">
+//                     <h3>{van.name}</h3>
+//                     <p>${van.price}<span>/day</span></p>
+//                 </div>
+//                 <i className={`van-type ${van.type} selected`}>{van.type}</i>
+//             </Link>
 //         </div>
-//     ));
+//     ))
 
 //     return (
-//         <div>
-//             {vanElements}
+//         <div className="van-list-container">
+//             <h1>Explore our van options</h1>
+//             <div className="van-list">
+//                 {vanElements}
+//             </div>
 //         </div>
-//     );
+//     )
 // }
-
-// export default Vans;
-
 import React from "react"
 import { Link } from "react-router-dom"
 
