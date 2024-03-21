3ccdb99e528f3d84b383d5467dfb96f620a08270
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-21 07:16:41 +0300

    Fix imports

--- index.jsx
+++ index.jsx
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Vans from "./pages/Vans";
-import VanDetail from "./pages/VanDetail";
-import Layout from "./components/Layout"; 
-
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import Layout from "./components/Layout"
 import './server'
 
 function App() {
@@ -19,6 +21,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </Router>
