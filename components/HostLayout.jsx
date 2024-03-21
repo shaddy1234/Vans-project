33eb8404e73220cd63ab8f4b2af61f03e7e50250
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-21 12:36:19 +0300

    Dynamic styles

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -1,15 +1,39 @@
 import React from "react"
-import { Link, Outlet } from "react-router-dom"
+import { NavLink, Outlet } from "react-router-dom"
 
 export default function HostLayout() {
+    const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
     return (
         <>
-            <nav className="host-nav">
-                <Link to="/host">Dashboard</Link>
-                <Link to="/host/income">Income</Link>
-                <Link to="/host/reviews">Reviews</Link>
-            </nav>
-            <Outlet />
-        </>
+        <nav className="host-nav">
+            <NavLink
+                to="/host"
+                end
+                style={({ isActive }) => isActive ? activeStyles : null}
+            >
+                Dashboard
+            </NavLink>
+
+            <NavLink
+                to="/host/income"
+                style={({ isActive }) => isActive ? activeStyles : null}
+            >
+                Income
+            </NavLink>
+
+            <NavLink
+                to="/host/reviews"
+                style={({ isActive }) => isActive ? activeStyles : null}
+            >
+                Reviews
+            </NavLink>
+
+        </nav>
+        <Outlet />
+    </>
     )
 }
