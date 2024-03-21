2e5f41aa9f671fd6d6a141edb0c016be647766a0
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-21 12:10:46 +0300

    Add dynamic styling

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <div>
       <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
         <nav>
-          <Link to="/host">Host</Link>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
+          <NavLink to="/host" className={({isActive}) => isActive ? "active-styles" : null}>
+            Host
+          </NavLink>
+
+          <NavLink to="/about" className={({isActive}) => isActive ? "active-styles" : null}>
+            About
+          </NavLink>
+
+          <NavLink to="/vans" className={({isActive}) => isActive ? "active-styles" : null}>
+            Vans
+          </NavLink>
         </nav>
       </header>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
