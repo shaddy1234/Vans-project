292a1931f0ded0ec38254bf6d5435743b57c4165
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-21 07:54:22 +0300

    Add links

--- index.jsx
+++ index.jsx
@@ -10,6 +10,7 @@ import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
 import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
 import './server'
 
 function App() {
@@ -21,9 +22,11 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </Router>
