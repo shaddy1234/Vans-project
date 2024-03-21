05666842819a2d6528db2bde1a5beeca16118791
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-21 07:17:25 +0300

    Add host link

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,6 +7,7 @@ function Header() {
       <header>
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
+          <Link to="/host">Host</Link>
           <Link to="/about">About</Link>
           <Link to="/vans">Vans</Link>
         </nav>
