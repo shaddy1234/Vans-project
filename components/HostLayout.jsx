756ba011b69b1169e1f19a92938dbefa7d5829eb
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-23 05:16:04 +0300

    Add vans link

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -26,6 +26,13 @@ export default function HostLayout() {
             </NavLink>
 
             <NavLink
+                to="/host/vans"
+                style={({ isActive }) => isActive ? activeStyles : null}
+            >
+                Vans
+            </NavLink>
+
+            <NavLink
                 to="/host/reviews"
                 style={({ isActive }) => isActive ? activeStyles : null}
             >
