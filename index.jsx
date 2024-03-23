d92c0ae19d6ba1f6c19d903d0e198e7788677d14
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-23 05:27:21 +0300

    Import files

--- index.jsx
+++ index.jsx
@@ -9,8 +9,11 @@ import VanDetail from "./pages/Vans/VanDetail"
 import Dashboard from "./pages/Host/Dashboard"
 import Income from "./pages/Host/Income"
 import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
 import Layout from "./components/Layout"
 import HostLayout from "./components/HostLayout"
+
 import './server'
 
 function App() {
@@ -27,6 +30,8 @@ function App() {
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
           </Route>
         </Route>
       </Routes>
