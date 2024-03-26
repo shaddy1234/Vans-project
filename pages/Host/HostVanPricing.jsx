0a174969463db926643f4d8892c16a1cb62d8247
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-26 10:25:12 +0300

    Update pricing

--- pages/Host/HostVanPricing.jsx
+++ pages/Host/HostVanPricing.jsx
@@ -1,7 +1,9 @@
 import React from "react"
+import { useOutletContext } from "react-router-dom"
 
 export default function HostVanPricing() {
+    const { currentVan } = useOutletContext()
     return (
-        <h2>Pricing info goes here</h2>
+        <h3 className="host-van-price">${currentVan.price}<span>/day</span></h3>
     )
 }
