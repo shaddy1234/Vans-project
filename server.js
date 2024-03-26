a94d4a867f604d42b7f03e63e062c4f92c7204f6
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-26 10:22:31 +0300

    Update server

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ createServer({
         this.get("/host/vans/:id", (schema, request) => {
             // Hard-code the hostId for now
             const id = request.params.id
-            return schema.vans.where({ id, hostId: "123" })
+            return schema.vans.findBy({ id, hostId: "123" })
         })
     }
 })
