ff2ccdc47b8f6e2abafc5aaddff3a9cfda77dbfe
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-26 10:23:55 +0300

    useContext

--- pages/Host/HostVanInfo.jsx
+++ pages/Host/HostVanInfo.jsx
@@ -1,7 +1,15 @@
 import React from "react"
+import { useOutletContext } from "react-router-dom"
 
 export default function HostVanInfo() {
+    const { currentVan } = useOutletContext()
+    
     return (
-        <h2>Detailed info goes here</h2>
+        <section className="host-van-detail-info">
+            <h4>Name: <span>{currentVan.name}</span></h4>
+            <h4>Category: <span>{currentVan.type}</span></h4>
+            <h4>Description: <span>{currentVan.description}</span></h4>
+            <h4>Visibility: <span>Public</span></h4>
+        </section>
     )
 }
