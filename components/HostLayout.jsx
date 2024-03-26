0573a46064454d7f4e5d53acac1a166fb306d5ab
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-26 09:04:12 +0300

    relative linking

--- components/HostLayout.jsx
+++ components/HostLayout.jsx
@@ -19,21 +19,21 @@ export default function HostLayout() {
             </NavLink>
 
             <NavLink
-                to="/host/income"
+                to="income"
                 style={({ isActive }) => isActive ? activeStyles : null}
             >
                 Income
             </NavLink>
 
             <NavLink
-                to="/host/vans"
+                to="vans"
                 style={({ isActive }) => isActive ? activeStyles : null}
             >
                 Vans
             </NavLink>
 
             <NavLink
-                to="/host/reviews"
+                to="reviews"
                 style={({ isActive }) => isActive ? activeStyles : null}
             >
                 Reviews
