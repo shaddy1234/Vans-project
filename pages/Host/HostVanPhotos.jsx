2e4abb69839a59d5cce250ec1d0191b87fffc371
Shadrack Kimaau <musyokishadrach1@gmail.com>
2024-03-26 10:24:33 +0300

    integrate with server

--- pages/Host/HostVanPhotos.jsx
+++ pages/Host/HostVanPhotos.jsx
@@ -1,7 +1,9 @@
 import React from "react"
+import { useOutletContext } from "react-router-dom"
 
 export default function HostVanPhotos() {
+    const { currentVan } = useOutletContext()
     return (
-        <h2>Photos go here</h2>
+        <img src={currentVan.imageUrl} className="host-van-detail-image" />
     )
 }
